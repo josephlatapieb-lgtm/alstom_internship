d5b0ba82fb0a1ffaf94cf57929bce773ff1bcbb1
Joseph latapie Bayroo <joseph.latapieb@gmail.com>
2026-05-22 13:54:48 +0200

    Mise à jour des fichiers de soumission

--- submission/my_policy.py
+++ submission/my_policy.py
@@ -1,3 +1,19 @@
-from submission.random_policy import RandomPolicy
+import torch
+from reinforcement_learning.my_policy import ActorCritic
 
-MyPolicy = RandomPolicy
+class MyPolicy:
+    def __init__(self):
+        self.agent = ActorCritic(obs_size=36, n_actions=5)
+
+        checkpoint = torch.load("submission/checkpoint.pt", map_location="cpu")
+        self.agent.load_state_dict(checkpoint["model"])
+        self.agent.eval()
+
+    def act(self, obs, env):
+        actions = {}
+        for h in env.get_agent_handles():
+            if obs[h] is not None:
+                actions[h] = self.agent.act(obs[h])
+            else:
+                actions[h] = 0
+        return actions
